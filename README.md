738275e8fcc3a28431e1b3ecafbeb21d023a5716
Vanessa Sochat <vsochat@stanford.edu>
2018-12-03 10:57:12 -0500

    adding first go, needs docs and testing

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 # Confucious Actions
 
-What could be better than a ludicous and sarcastic comment when you fail a pull
-request? With this action, that's what you get. It uses the 
+What could be better than a ludicous and sarcastic comment when you perform
+some action on a Github pull request? This repository provides those actions,
+with specific action types corresponding to the subfolders. We currently have:
+
+ - [Pull Request Failure](pull_request_fail): Some wisdom when a pull request has status "failure"
+
+These actions use the 
 [vanessa/boxes](https://www.github.com/vsoch/boxes) container to format
 a lovely message from Confucious to your pull requests, but only if you fail.
+See the README.md in each subfolder for usage.
 
 **under development**
---
+++ pull_request_fail/fail.sh
@@ -0,0 +1,160 @@
+#!/bin/bash
+
+# Suggested by Github actions to be strict
+set -e
+set -o pipefail
+
+################################################################################
+# Global Variables (we can't use GITHUB_ prefix)
+################################################################################
+
+API_VERSION=v3
+BASE=https://api.github.com
+AUTH_HEADER="Auth
+orization: token ${GITHUB_TOKEN}"
+HEADER="Accept: application/vnd.github.${API_VERSION}+json;"
+HEADER="${HEADER}; application/vnd.github.antiope-preview+json"
+
+# URLs
+REPO_URL="${BASE}/repos/${GITHUB_REPOSITORY}"
+RUNS_URL=${REPO_URL}/commits/${GITHUB_SHA}/check-runs # Runs, harhar
+
+################################################################################
+# Helper Functions
+################################################################################
+
+response_fail() {
+
+    echo "Error with token or response.";
+    exit 1;
+
+}
+
+get_url() {
+
+    echo $(curl -sSL -H "${AUTH_HEADER}" -H "${API_HEADER}" "${1:-}")
+}
+
+check_credentials() {
+
+    if [[ -z "${GITHUB_TOKEN}" ]]; then
+        echo "You must include the GITHUB_TOKEN as an environment variable."
+        exit 1
+    fi
+
+    # Validate the GitHub token.
+    curl -o /dev/null -sSL -H "${AUTH_HEADER}" -H "${HEADER}" "${REPO_URL}"; || response_fail
+
+}
+
+check_events_json() {
+
+    if [[ ! -f "${GITHUB_EVENT_PATH}" ]]; then
+        echo "Cannot find Github events file at ${GITHUB_EVENT_PATH}";
+        exit 1;
+    fi
+
+}
+
+clean_up() {
+
+    # Get all the comments for the pull request.
+    BODY=$(get_url "${COMMENTS_URL}")
+    COMMENTS=$(echo "$BODY" | jq --raw-output '.[] | {id: .id, body: .body} | @base64')
+
+    for C in ${COMMENTS}; do
+        COMMENT="$(echo "$C" | base64 --decode)"
+        COMMENT_ID=$(echo "$COMMENT" | jq --raw-output '.id')
+        COMMENT_BODY=$(echo "$comment" | jq --raw-output '.body')
+
+        # All Confuscious posts starts with this tag
+	if [[ "$COMMENT_BODY" == *"GitHub Confucious Action Say"* ]]; then
+            echo "Deleting old comment ID: $id"
+            curl -sSL -H "${AUTH_HEADER}" -H "${HEADER}" -X DELETE "${REPO_URL}/issues/comments/${id}"
+        fi
+    done
+}
+
+post_message() {
+
+    WISDOM_FILE=$(mktemp /tmp/wisdom.XXXXXXXXX)
+    echo "GitHub Confucious Action Say:" > ${WISDOM_FILE}
+    /entrypoint.sh --message >> ${WISDOM_FILE}
+    WISDOM=$(cat ${WISDOM_FILE})
+
+    curl -sSL -H "${AUTH_HEADER}" -H "${HEADER}" -d '{"body":"${WISDOM}"}' -H "Content-Type: application/json" -X POST "${COMMENTS_URL}"
+}
+
+check_runs() {
+
+    RESPONSE=$(get_url "${RUNS_URL}")
+    RUNS=$(echo "${RESPONSE}" | jq --raw-output '.check_runs | .[] | {name: .name, status: .status, conclusion: .conclusion} | @base64')
+
+    # We have to keep looping if in progress
+    INPROGRESS=0
+
+    # We need to cycle through actions, and:
+    # 1. Skip those that are this action
+    # 2. Find if there is a confucious action in progress
+    for R in ${RUNS}; do
+        RUN="$(echo "${R}" | base64 --decode)"
+
+        # Case 1: Skip this action
+        NAME=$(echo "${RUN}" | jq --raw-output '.name')
+	if [[ "${GITHUB_ACTION}" == "${NAME}" ]]; then
+            continue
+	fi
+
+        # Case 2: Is it in progress?
+	STATE=$(echo "${RUN}" | jq --raw-output '.status')
+        if [[ "${STATE}" == "in_progress" ]]; then
+            INPROGRESS=1
+            continue
+        fi
+
+        # Case 3: Did we FAIL
+	RESULT=$(echo "${RUN}" | jq --raw-output '.conclusion')
+        if [[ "${STATE}" == "completed" ]] && [[ "$RESULT" == "failure" ]]; then
+            echo "Run: $NAME failure! Whomp whomp. Confuscious says..."
+            clean_up;
+	    post_message;
+            exit 0
+        fi
+
+	# If we got in progress checks then sleep and loop again.
+        if [[ "${INPROGRESS}" == "1" ]]; then
+            echo "A watched pot never boils! Sleeping..."
+            sleep 3
+
+            # Continue calling self until we exit.
+            check_runs;
+	fi
+
+    done
+
+    # If we make it here, we didn't hit a fail result, or in progress
+    # Delete the confuscious comment if it exists to refresh the pull request
+    clean_up;
+}
+
+main () {
+
+    # path to file that contains the POST response of the event
+    # Example: https://github.com/actions/bin/tree/master/debug
+    # Value: /github/workflow/event.json
+    check_events_json
+
+    # Get the name of the action that was triggered
+    ACTION=$(jq --raw-output .action "${GITHUB_EVENT_PATH}")
+    NUMBER=$(jq --raw-output .number "${GITHUB_EVENT_PATH}")
+    COMMENTS_URL=${REPO_URL}/issues/${NUMBER}/comments
+
+    # Only interested in newly opened 
+    # https://developer.github.com/v3/activity/events/types/#pullrequestevent
+    if [[ "${ACTION}" == "opened" ]]; then
+        check_credentials
+        check_runs
+    fi
+}
+
+main
